9d16e283c0d4da40664208b5a591dc6ab0ef26ba
Conor Manning <conormanning13@gmail.com>
2025-05-24 09:33:38 -0500

    ssh key creation in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/bash
 
+SSH_KEY='ansibletest'
+SSH_COMMENT='wallace@conorwithonen.com'
+
 if [ ! -d ./venv ]; then
+  echo "Creating virtual environment"
   python3 -m venv venv && source venv/bin/activate
   pip install -r requirements.txt
+fi
+
+if [ ! -e ./ssh/$SSH_KEY ];then
+  echo "No SSH found. Creating one"
+  ssh-keygen -t ed25519 -b 4096 -C $SSH_COMMENT -N '' -f ./ssh/$SSH_KEY
 fi
 
 # clear up existing inventory files
